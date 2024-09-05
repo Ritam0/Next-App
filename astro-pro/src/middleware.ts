a685659724bd45e1090aa26ef762b56e8d5d7a83
Ritam Majumder <ritmmajumder0@gmail.com>
2024-09-06 01:04:59 +0530

    middleware updated

--- astro-pro/src/middleware.ts
+++ astro-pro/src/middleware.ts
@@ -1,46 +1,46 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
+// This function can be marked `async` if using `await` inside
 
-export { default } from 'next-auth/middleware';
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request });
-
-    const url = request.nextUrl;
-
-    // If the user is authenticated, redirect them away from auth pages
-    if (token && 
+    const token=await getToken({req:request});
+    const url=request.nextUrl
+    if(token && 
         (
-            url.pathname.startsWith('/sign-in') || 
-            url.pathname.startsWith('/sign-up') || 
+            url.pathname.startsWith('/sign-in') ||
+            url.pathname.startsWith('/sign-up') ||
             url.pathname.startsWith('/verify')
-        )) {
-        return NextResponse.redirect(new URL('/dashboard', request.url));
+    
+        )
+    
+    ){
+        return NextResponse.redirect(new URL('/dashboard', request.url))
     }
 
-    // If the user is not authenticated, restrict access to protected pages
-    if (!token && 
+    if(!token && 
         (
-            url.pathname.startsWith('/dashboard') || 
-            url.pathname.startsWith('/astro') || 
+            url.pathname.startsWith('/dashboard') ||
+            url.pathname.startsWith('/astro') ||
             url.pathname.startsWith('/verify')
-        )) {
-        return NextResponse.redirect(new URL('/sign-in', request.url));
+    
+        )
+    
+    ){
+        return NextResponse.redirect(new URL('/sign-in', request.url))
     }
-
-    // Allow the request to proceed
-    return NextResponse.next();
+  //return NextResponse.redirect(new URL('/home', request.url))
 }
-
-// Apply the middleware to specific paths
+ 
+// See "Matching Paths" below to learn more
 export const config = {
-    matcher: [
-        '/sign-in',
-        '/sign-up',
-        '/',
-        '/dashboard/:path*',
-        '/verify/:path*',
-        '/astro/:path*',
-    ],
-};
+  matcher: [
+    '/sign-in',
+    '/sign-up',
+    '/',
+    '/dashboard/:path*',
+    '/verify/:path*'
+  ]
+}
