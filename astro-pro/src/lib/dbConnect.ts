e61403b0c96119585f86601faff1db78bd83036d
Ritam Majumder <ritmmajumder0@gmail.com>
2024-09-06 23:41:29 +0530

    db name set

--- astro-pro/src/lib/dbConnect.ts
+++ astro-pro/src/lib/dbConnect.ts
@@ -1,3 +1,4 @@
+"use server"
 import mongoose from "mongoose";
 
 type ConnectionObject={
@@ -8,7 +9,9 @@ const connection:ConnectionObject={}
 async function dbConnect():Promise<void>{
    
     try{
-        const db=await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://ritammajumder2025:i8msBfRjIdAvyUvX@astroauth.uw5cm.mongodb.net/?retryWrites=true&w=majority&appName=astroAuth')
+        const db=await mongoose.connect(process.env.MONGO_URI || '',{
+            dbName:"test"
+        })
         connection.isConnected=db.connections[0].readyState
         console.log("Data Base Connected")
     }catch(c){
