65bd241c12f624bc0078f7b1cbb7244def8efc7b
Ritam Majumder <ritmmajumder0@gmail.com>
2024-09-06 18:42:45 +0530

    db connect issue

--- astro-pro/src/lib/dbConnect.ts
+++ astro-pro/src/lib/dbConnect.ts
@@ -8,7 +8,7 @@ const connection:ConnectionObject={}
 async function dbConnect():Promise<void>{
    
     try{
-        const db=await mongoose.connect(process.env.MONGO_URI || '')
+        const db=await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://ritammajumder2025:i8msBfRjIdAvyUvX@astroauth.uw5cm.mongodb.net/?retryWrites=true&w=majority&appName=astroAuth')
         connection.isConnected=db.connections[0].readyState
         console.log("Data Base Connected")
     }catch(c){
